dbba71eea9372950f27cec401cae081389c0ecec
kashyappriyanshu0123 <93027668+kashyappriyanshu0123@users.noreply.github.com>
2026-04-17 16:51:30 +0530

    Update database password comment in db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,6 @@ import mysql from "mysql2/promise";
 export const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "kashyap1234", // change this
+  password: "kashyap1234", 
   database: "subscription_db",
 });
